d27935ec8ff4e10b39af941b880995344995763c
Caique Goulart <70335792+Caiquesgoulart@users.noreply.github.com>
2020-09-01 12:47:55 -0300

    Add files via upload

---
+++ busca_Binaria.py
@@ -0,0 +1,34 @@
+import time
+
+def busca_binaria(array, buscar):
+    i = 0
+    f = len(array) - 1
+    meio = 0
+    while i <= f:
+        meio = (f + i) // 2
+        if array[meio] < buscar:
+            i = meio + 1
+        elif array[meio] > buscar:
+            f = meio - 1
+        else:
+            return meio
+
+    return -1
+
+
+array = [1, 5, 8, 15, 27, 30, 45, 67, 75, 84, 97, 108, 117, 123, 134]
+print('', array)
+
+buscar = int(input('Entre com um numero: '))
+posicao = busca_binaria(array, buscar)
+
+if posicao != -1:
+    print('Elemento ', buscar, ' encontrado na posição: ', posicao)
+else:
+    print('Elemento ', buscar, ' não existe')
+
+tempo = time.time() // 1000000
+print('Tempo levado: ', tempo, ' milissegundos')
+
+print('Enter pra fechar...')
+input()
